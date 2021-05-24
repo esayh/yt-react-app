081ac64ebbe1b6775ef10faa24edf6d6c12f0cc3
Ivan Castillo <total180training@gmail.com>
2021-05-24 17:15:00 -0400

    Video function component turned to class

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,19 +1,52 @@
+import axios from 'axios'
 import React from 'react'
 import YouTube from 'react-youtube'
 
 
-function Video(props) {
+class Video extends React.Component {
 
-    const { id } = props.match.params
-    console.log(id)
+    constructor(props) {    // Called props into constructor so that we can retrieve id inside the constructor
+        super()
+        this.state = {
+            id: props.match.params.id,
+            video: {}
+        }
+    }
+    componentDidMount() {
+        this.getVideoDetails()
+        console.log('componentDidMount')
+    }
 
+    getVideoDetails = async () => { // This function should get video data so that we may display it under video
+        const { id, video } = this.state
+        const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?id=${id}&part=snippet,contentDetails,statistics&key=${process.env.REACT_APP_API_KEY}`)
+        
+        this.setState({
+            video: data.items[0]
+        })
+    }
     // Write function that takes and makes comments
+    
 
-    return (
-        <div>
-            <YouTube videoId={id} />
-        </div>
-    )
+    render() {
+        const { id, video } = this.state
+        console.log('render')
+        try {
+            console.log(video.snippet.title)
+        }
+        catch(error) {
+            console.log('could not print video.title')
+        }
+
+            return (
+                <div>
+                    <YouTube videoId={id} />
+                    {/* <h2>{video.snippet.title}</h2> */}
+                </div>
+            )
+    }
+
 }
 
 export default Video
+
