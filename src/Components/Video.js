ca2e405ad1817f1e8dfe895ab19055fc081d228a
Ivan Castillo <total180training@gmail.com>
2021-05-26 10:27:57 -0400

    Added video information on Video component

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -8,26 +8,42 @@ class Video extends React.Component {
     constructor(props) {    // Called props into constructor so that we can retrieve id inside the constructor
         super()
         this.state = {
-            id: props.match.params.id,
-            video: {}
+            videoId: props.match.params.id,
+            video: {},
+            channelId: '',
+            channel: {}
         }
     }
     componentDidMount() {
-        this.getVideoDetails()
         console.log('componentDidMount')
+        this.getVideoDetails()
+        this.getChannelDetails()
     }
 
     getVideoDetails = async () => { // This function should get video data so that we may display it under video
-        const { id } = this.state
+        const { videoId } = this.state
 
         try {
-            const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?id=${id}&part=snippet,contentDetails,statistics&key=${process.env.REACT_APP_API_KEY}`)
+            const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet,contentDetails,statistics&key=${process.env.REACT_APP_API_KEY}`)
             this.setState({
-                video: data.items[0]
+                video: data.items[0],
+                channelId: data.items[0].snippet.channelId
             })
         }
         catch (e) {
             console.log('Video could not be found')
+        }
+    }
+    getChannelDetails = async () => {
+        const { channelId } = this.state
+        console.log(channelId)
+        // const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/channels?id=${channelId}&part=snippet&key=${process.env.REACT_APP_API_KEY}`)
+        try {
+        const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${process.env.REACT_APP_API_KEY}`)
+        console.log(data.items[0])
+        }
+        catch {
+            console.log('could not find channel')
         }
     }
     // Write function that takes and makes comments
@@ -35,7 +51,7 @@ class Video extends React.Component {
     
 
     render() {
-        const { id, video } = this.state
+        const { videoId, video } = this.state
         console.log('render')
         try {
             console.log(video.snippet.title)
@@ -46,10 +62,19 @@ class Video extends React.Component {
 
             return (
                 <div className='Video'>
-                    <YouTube videoId={id} />
+                    <YouTube videoId={videoId} />
                     <div>
+                        {/* This div is for the  */}
                         {video.snippet ? <h2>{video.snippet.title}</h2> : null}
-                        {/* {video.statistics} */}
+                        {video.statistics ? <span><p>{Number(video.statistics.viewCount).toLocaleString()} views</p></span> : null}
+                        {video.statistics ? <span><p>{Number(video.statistics.likeCount).toLocaleString()} Likes | {Number(video.statistics.dislikeCount).toLocaleString()} Dislikes</p></span> : null}
+                    </div>
+                    <div>
+                        {/* Channel logo, Channel name */}
+                        {video.snippet ? <p>{video.snippet.channelTitle} Channel</p> : null}
+                        {/*channel subscribers*/}
+                        {/* Description */}
+                        {video.statistics ? video.snippet.description : null}
                     </div>
                 </div>
             )
@@ -58,4 +83,3 @@ class Video extends React.Component {
 }
 
 export default Video
-
